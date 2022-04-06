5d9c04fd014246640fa83900973307629ed067f8
Paulo Bordignon <p.h_@outlook.com>
2022-04-05 22:09:38 -0300

    feat(all): added eslint, prettierrc and started the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>starting...</p>
+    <div className="faucet-wrapper">
+      <div className="faucet">
+        <div className="balance-view is-size-2">
+          Current balance: <strong> 2 </strong> ETH
+        </div>
+        <button className="btn mr-2" type="button">
+          Donate
+        </button>
+        <button className="btn" type="button">
+          Withdraw
+        </button>
+      </div>
     </div>
   );
 }
